4a8f3c9331f1455a70bf7f15d209c6fae4034270
Shuang zhou <shuangzhou@wustl.edu>
2022-08-10 16:57:24 -0500

    angle unit is rad

--- matlab_func/dose3d_mex.m
+++ matlab_func/dose3d_mex.m
@@ -11,5 +11,5 @@
 % para = [A1,mux1,muy1,sigma1, A2,mux2,muy2,sigma2,...], for isotropic
 % para = [A1,mux1,muy1,sigma1,sigma2,beta1, A2...], for anisotropic
 % dose_mex = dose3d_mex(x,y,para,Nz,N_gaussian);
-% anisotropic example: dose_mex = dose3d_mex(((1:128)-64.5)*0.2,((1:128)-64.5)*0.2,[1,2,4,1,4,45, 1,-2,-4,4,1,15],1,2);
+% anisotropic example: dose_mex = dose3d_mex(((1:128)-64.5)*0.2,((1:128)-64.5)*0.2,[1,2,4,1,4,45*pi/180, 1,-2,-4,4,1,15*pi/180],1,2);
 % isotropic example: dose_mex = dose3d_mex(((1:128)-64.5)*0.2,((1:128)-64.5)*0.2,[1,2,4,1, 1,-2,-4,2],1,2);
